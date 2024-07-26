b60b9b794d4f1ac7f0464c8ddfb3c03c36a81c07
William Brixius <wjbrixius@gmail.com>
2024-07-26 16:50:23 -0400

    Update 2stair_proc.py for compatibility with Py3 (Py2 compatibility retained)
    Just added () around print arguments, and added the input/raw_input handling.

--- 2stair_proc.py
+++ 2stair_proc.py
@@ -1,79 +1,75 @@
 def staircase( repeats, threshold, step, correct):
 	#first: constants
 	kStepMin = 1
-
 	kThreshMin = 20
-
 	kStepMax = 30
-
 	kThreshMax = 80
+
 	#correctInt is 1 if stimuli detected, else -1
 	if correct:
 		correctInt = 1
 	else:
 		correctInt = -1
+
 	#decrease step size if reversal
 	if ((repeats > 0 and correctInt < 0) or (repeats < 0 and correctInt > 0)) :		
 		step = step / 2
 		if abs(repeats) > 3:
-			print "reversal after DOUBLING!"
+			print("reversal after DOUBLING!")
 			repeats = 0
 		else:
-			print "reversal without prior doubling"
+			print("reversal without prior doubling")
 			repeats = correctInt
 	else:
 		if repeats == 0: #very first trial - no previous doubling	
 			repeats = repeats + correctInt 
 	repeats = repeats + correctInt
+
 	#increase step size for repeated responses
 	if abs(repeats) > 3:
-		print "Three or more in same direction DOUBLING!"
+		print("Three or more in same direction DOUBLING!")
 		step = step * 2	
 	if step < kStepMin:
-
 		step = kStepMin
-
 	if step > kStepMax:
-
 		step = kStepMax
+
 	#now adjust threshold
 	prevThreshold = threshold
 	threshold = threshold - (step * correctInt)
 	if threshold < kThreshMin:
-
 		threshold = kThreshMin
-
 	if threshold > kThreshMax:
-
 		threshold = kThreshMax
 	step = abs(prevThreshold - threshold)
-	print "  threshold %.2f, stepsize %.2f, repeats %d" % (threshold, step, repeats)
+	print("  threshold %.2f, stepsize %.2f, repeats %d" % (threshold, step, repeats))
 	return repeats, threshold, step
-
 
 #####Main Loop
 
 #Initialize staircase
-
 repeats = 0
 threshold = 80
 step = 15
 
+#Python 2/3 dual compatibility -- 3 uses input, 2 uses raw_input, this should
+#allow both to be happy.
+try:
+    input = raw_input
+except NameError:
+    pass
+
 #Titrate across trials
-
 for trial in range(0, 150): #allowing up to 150 trials before program breaks
-
 	response = raw_input("Trial %d: Does participant detect a stimulus with a contrast of  %.2f? (y/n)\n" % (trial, threshold))
-
 	if response.lower() in ("y", "yes"):
-
      		Correct = True
-
 	else:
-
 		Correct = False
 	repeats, threshold, step = staircase( repeats, threshold, step, Correct)
 
+##Oh look, an ancient note from grad school past -- I'll leave it here, with
+##this new note.
 #What do we need to pass back and forth with EB?
 #Increase or Decrease of contrast (if threshold < 50, decrease.  if threshold > 50, increase)
 #Correct or Incorrect response
